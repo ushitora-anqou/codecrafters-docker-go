58b8da2ac67778ebb0d895a0489b3c07d1c93d12
Ushitora Anqou <ushitora@anqou.net>
2023-01-06 11:40:01 +0900

    #2

--- app/main.go
+++ app/main.go
@@ -1,25 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"io"
+	"log"
 	"os"
 	"os/exec"
 )
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
 
 	cmd := exec.Command(command, args...)
-	output, err := cmd.Output()
+	stdout, err := cmd.StdoutPipe()
 	if err != nil {
-		fmt.Printf("Err: %v", err)
-		os.Exit(1)
+		log.Fatal(err)
 	}
-
-	fmt.Println(string(output))
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+	io.Copy(os.Stdout, stdout)
+	io.Copy(os.Stderr, stderr)
 }
